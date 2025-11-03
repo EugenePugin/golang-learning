52061c0acb5afecb744b85958ea9079842ba740c
Eugene Pugin <6271799+EugenePugin@users.noreply.github.com>
2025-11-03 19:28:25 +0300

    #

--- Learning-Go/hello-world.go
+++ Learning-Go/hello-world.go
@@ -4,30 +4,28 @@ import (
 	"fmt"
 )
 
-func one11a(dbgMode int) {
-	var R float64
+func one12a(dbgMode uint) {
+	var workArray [16]uint
 
 	if dbgMode == 1 { //dbg mode
-		R = 1000001
-		// -000012.2123
-		//  12.12345678
-	} else { //release mode
-		fmt.Scan(&R)
+		workArray = [16]uint{99, 151, 137, 71, 117, 187, 20, 93, 187, 67, 1, 2, 3, 5, 7, 8}
+	} else {
+
 	}
 
-	if R <= 0 {
-		formattedString := fmt.Sprintf("%4.2f", R)
-		fmt.Println("число", formattedString, "не подходит")
-	} else if R > 10000 {
-		formattedString := fmt.Sprintf("%e", R)
-		fmt.Println(formattedString)
-	} else {
-		convertedR := (float64)((int64)(R*R*10000)) / 10000
-		formattedString := fmt.Sprintf("%.4f", convertedR)
-		fmt.Println(formattedString)
-	}
+	// if R <= 0 {
+	// 	formattedString := fmt.Sprintf("%4.2f", R)
+	fmt.Println(workArray)
+	// } else if R > 10000 {
+	// 	formattedString := fmt.Sprintf("%e", R)
+	// 	fmt.Println(formattedString)
+	// } else {
+	// 	convertedR := (float64)((int64)(R*R*10000)) / 10000
+	// 	formattedString := fmt.Sprintf("%.4f", convertedR)
+	// 	fmt.Println(formattedString)
+	// }
 }
 
 func main() {
-	one11a(0)
+	one12a(1)
 }
