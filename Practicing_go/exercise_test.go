959ea9ff519b15253e3b6846b44f10cdbff10bbf
Eugene Pugin <6271799+EugenePugin@users.noreply.github.com>
2026-04-14 20:09:00 +0300

    lc8

--- Practicing_go/exercise_test.go
+++ Practicing_go/exercise_test.go
@@ -33,6 +33,11 @@ func Test_removeDuplicates(t *testing.T) {
 			5,
 		},
 		{
+			"simple test {2, 2, 2,3,2,2}",
+			[]int{2, 2, 2, 3, 2, 2},
+			2,
+		},
+		{
 			"simple test {0,0,1,1,1,2,2,3,3,4}",
 			[]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4},
 			5,
