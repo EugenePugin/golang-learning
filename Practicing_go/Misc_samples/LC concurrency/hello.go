caf1195fa1eab9bf95423bbb5b1743bd5cf3e5b3
Eugene Pugin <6271799+EugenePugin@users.noreply.github.com>
2026-05-12 16:56:00 +0300

    LC concurrency

--- Practicing_go/Misc_samples/LC concurrency/hello.go
+++ Practicing_go/Misc_samples/LC concurrency/hello.go
@@ -1,4 +1,4 @@
-package main
+package LC_concurrency
 
 import (
 	"fmt"
--- Practicing_go/exercise.go
+++ Practicing_go/exercise.go
@@ -1,32 +1,4 @@
-// https://leetcode.com/problems/reverse-integer/description/
+// https://leetcode.com/problems/print-in-order/description/
 
 package main
 
-import (
-	"math"
-)
-
-func reverse(x int) int {
-	var digit, result int
-	for x != 0 {
-		digit = x % 10
-		x /= 10
-
-		if (result > math.MaxInt32/10) ||
-			(result == math.MaxInt32/10 && digit > 7) {
-			// fmt.Println("overflow danger")
-			return 0
-		}
-
-		if (result < math.MinInt32/10) ||
-			((result == math.MinInt32/10) && (digit < -8)) {
-			// fmt.Println("overflow danger")
-			return 0
-		}
-
-		result = result*10 + digit
-		// fmt.Println(digit, x, result)
-	}
-
-	return result
-}
