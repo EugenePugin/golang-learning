1a7f81e8dc97f9e5cfef4a987e7f117931e89429
Eugene Pugin <6271799+EugenePugin@users.noreply.github.com>
2026-05-15 20:26:18 +0300

    idiomatic way

--- Practicing_go/exercise.go
+++ Practicing_go/exercise.go
@@ -2,50 +2,43 @@
 
 package main
 
+import "math"
+
 func myAtoi(s string) int {
-	var result, tmp int
+	var result int64
+	var digit int64
 	isNegative := false
 	isLeading := true
-	var symbol string
-	for i := range len(s) {
-		symbol = string(s[i])
-		switch symbol {
-		case " ":
-			if !isLeading {
-				goto outFromTheLoop //quit in case of not leading
-			}
+	for i := range s {
+		switch {
+		case s[i] == ' ' && isLeading:
 			continue
-		case "+":
-			if !isLeading {
-				goto outFromTheLoop //quit in case of not leading
-			}
+		case (s[i] == '+' || s[i] == '-') && isLeading:
 			isLeading = false
+			isNegative = (s[i] == '-')
 			continue
-		case "-":
-			if !isLeading {
-				goto outFromTheLoop //quite in case of not leading
+		case s[i] >= '0' && s[i] <= '9':
+			isLeading = false
+			digit = int64(s[i]) - '0' //используем кодировку UTF-8
+			if isNegative {
+				if result > (-int64(math.MinInt32)-digit)/10 {
+					return math.MinInt32
+				}
+			} else {
+				if result > (int64(math.MaxInt32)-digit)/10 {
+					return math.MaxInt32
+				}
 			}
-			isLeading = false
-			isNegative = true
-			continue
-		case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
-			isLeading = false
-			tmp = int(s[i]) - 48 //используем кодировку UTF-8
-		default: //non digit symbol detected
-			goto outFromTheLoop
+
+			result = 10*result + digit
+
+		default: //non digit symbol either non-leading sign detected
+			goto done
 		}
-		// math.MaxInt32
-		if !isNegative && (1<<31-1)-10*result < tmp {
-			result = 1<<31 - 1
-		} else if isNegative && 1<<31-10*result < tmp {
-			result = 1 << 31
-		} else {
-			result = 10*result + tmp
-		}
-	}
-outFromTheLoop:
+	} // loop cycle
+done:
 	if isNegative {
-		result *= -1
+		return int(-result)
 	}
-	return result
+	return int(result)
 }
--- Practicing_go/hello.go
+++ Practicing_go/hello.go
@@ -2,50 +2,10 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
 	s := "2147483646"
 	//  2147483647
 	fmt.Println(myAtoi(s))
-}
-
-func myAtoi(s string) int {
-	var result int64
-	var digit int64
-	isNegative := false
-	isLeading := true
-	for i := range s {
-		switch {
-		case s[i] == ' ' && isLeading:
-			continue
-		case (s[i] == '+' || s[i] == '-') && isLeading:
-			isLeading = false
-			isNegative = (s[i] == '-')
-			continue
-		case s[i] >= '0' && s[i] <= '9':
-			isLeading = false
-			digit = int64(s[i]) - '0' //используем кодировку UTF-8
-			if isNegative {
-				if result > (-int64(math.MinInt32)-digit)/10 {
-					return math.MinInt32
-				}
-			} else {
-				if result > (int64(math.MaxInt32)-digit)/10 {
-					return math.MaxInt32
-				}
-			}
-
-			result = 10*result + digit
-
-		default: //non digit symbol either non-leading sign detected
-			goto done
-		}
-	} // loop cycle
-done:
-	if isNegative {
-		return int(-result)
-	}
-	return int(result)
 }
